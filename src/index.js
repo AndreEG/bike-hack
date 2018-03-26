ed5b9ad2953f15e5d0de0444868e038409413d61
SIlviaTivi <skfl.erg@hotmail.com>
2018-03-26 18:13:09 -0500

    configuracion firebase

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
+/* silvia */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
+/* import './index.css'; */
+import App from './components/App';
+/* import registerServiceWorker from './registerServiceWorker';
+ */
 
-/* import { createStore } from 'redux'; */
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import App from './components/app';
+firebase.initializeApp({
+    apiKey: "AIzaSyACUMEFB5mNIE9E_oYfzC1Qg8dD80SyVwc",
+    authDomain: "hackatonbikesantiago.firebaseapp.com",
+    databaseURL: "https://hackatonbikesantiago.firebaseio.com",
+    projectId: "hackatonbikesantiago",
+    storageBucket: "hackatonbikesantiago.appspot.com",
+    messagingSenderId: "482534686193"
+})
 
+ReactDOM.render(<App/>, document.getElementById('root'));
+/* registerServiceWorker();
+ */
+/* silvia */
 
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  
-    <App />
-  
-  , document.getElementById('root'))
