bfef68dc038a049ad07e691636b7ed84983a0651
LilliamHaro <qualia950@gmail.com>
2018-03-28 16:11:06 -0500

    cambiando colores de letras

--- src/components/acceso.js
+++ src/components/acceso.js
@@ -7,10 +7,10 @@ const Acceso = ({navigateTo}) => (
   <div className="Center">
      <br/>
     <h3 className="text-center white">Tu código: SB-1421</h3><br/><br/><br/>
-    <button className="btn-acc imgacc1">Mi tiempo <img className="icoacc" src="https://image.ibb.co/bUf1qS/reloj.png" alt="reloj" /></button>
+    <button className="btn-acc imgacc1 color-black">Mi tiempo <img className="icoacc" src="https://image.ibb.co/bUf1qS/reloj.png" alt="reloj" /></button>
     <br/>
-    <button onClick={() => navigateTo('pagina-6')} className="btn-acc imgacc2">Mapa <img className="icoacc"  src="https://image.ibb.co/cPhsx7/mapa.png" alt="mapa" /></button>
-    <button className="btn-acc imgacc2">Otra Bici <img className="icoacc"  src="https://image.ibb.co/ft5qjn/bici.png" alt="bici" /></button>
+    <button onClick={() => navigateTo('pagina-6')} className="btn-acc imgacc2 color-black">Mapa <img className="icoacc"  src="https://image.ibb.co/cPhsx7/mapa.png" alt="mapa" /></button>
+    <button className="btn-acc color-black imgacc2">Otra Bici <img className="icoacc"  src="https://image.ibb.co/ft5qjn/bici.png" alt="bici" /></button>
   </div>
  
 );
